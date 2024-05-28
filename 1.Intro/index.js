8bf5e90258e5d554d0753a5e5277e0cba562f461
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2024-05-28 16:32:51 +0300

    update

--- 1.Intro/index.js
+++ 1.Intro/index.js
@@ -1,2 +1,12 @@
-const { print } = require('./utils');
+const { print, getUsers } = require('./utils');
 print("Hello, NodeJS!")
+
+async function main(){
+    try {
+        const data = await getUsers();
+        console.log(data);
+    } catch (error) {
+        console.error(error.message)
+    }
+}
+main();
--- 1.Intro/utils.js
+++ 1.Intro/utils.js
@@ -2,4 +2,18 @@ function print(msg){
     console.log(msg);
 }
 
-module.exports = { print };
+async function getUsers(){
+    const api = "https://jsonplaceholder.typicode.com/users";
+    try {
+        const response = await fetch(api);
+        if (!response.ok){
+            throw new Error(response.statusText);
+        }
+        const data = await response.json();
+        return data;
+    }catch (error){
+        console.error("Error fetching users:", error);
+    }
+} 
+
+module.exports = { print, getUsers};
