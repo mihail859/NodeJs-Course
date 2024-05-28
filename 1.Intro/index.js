05fc659611042ef4823dcf8677df5e3f9ada9f1b
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2024-05-28 16:35:39 +0300

    Update index.js

--- 1.Intro/index.js
+++ 1.Intro/index.js
@@ -4,7 +4,7 @@ print("Hello, NodeJS!")
 async function main(){
     try {
         const data = await getUsers();
-        console.log(data);
+        console.log(data[0]);
     } catch (error) {
         console.error(error.message)
     }
